462d17f3369fa11fff99c35c916d150f745d5d97
iceklue <iceklue@gmail.com>
2018-05-13 22:44:05 +0200

    Finished 11

--- U11_HTML_Maximiliam_Berggren/script.js
+++ U11_HTML_Maximiliam_Berggren/script.js
@@ -4,8 +4,7 @@
  * reda på textnoden till paragrafen du håller 
  * på att skriva till
  */
- var currentText = "";
-
+ var currentText = null;
  
 /*
  * Du bör också ha en variabel som håller reda på
@@ -13,9 +12,6 @@
  * kan märka upp dem med klasserna even/odd
  */
 var numParagraphs = 0;
-
-
-
 
 /* 
  * Den här funktionen anropas då man trycker på en 
@@ -25,7 +21,8 @@ var numParagraphs = 0;
  * Funktionsparametrar:
  * c - Variabel som innehåller ett tecken
  */
-function addLetter(c){
+function addLetter(c)
+{
 	//Detta ska tas bort innan inlämning
 	alert("Pressed letter: '"+c+"'");
 
@@ -36,17 +33,18 @@ function addLetter(c){
 	 if(currentText==null)
 		 addParagraph();
 
-
 	/*
 	 * Här bör du lägga in tecknet c till den sparade textnoden
 	 */
+	 currentText.appendData(c);
 }
 
 /*
  * Den här funktionen anroas då man trycker
  * på Enter knappen
  */
-function addParagraph() {
+function addParagraph() 
+{
 	//Detta ska tas bort innan inlämning
 	alert("Add new paragraph");
 
@@ -54,17 +52,16 @@ function addParagraph() {
 	 * Här skapar du en ny paragraf med tillhörande textnod. 
 	 * Textnoden sparar du undan till din globala textnodsvariabel.
 	 */
-	 var op = document.getElementById("output");
-	 //var newParagraph = document.createElement("P");
-	// currentText = document.createTextNode("This is a paragraph.");
-	
+	 var newParagraph = document.createElement("P");
+	 currentText = document.createTextNode("");
+	 newParagraph.appendChild(currentText);
 
 	/*
 	 * Sedan så lägger du till paragrafen till DOM trädet till 
 	 * div-taggen som har id:t "output"
 	 */
-	 op.appendChild(	 document.createElement("P").appendChild(document.createTextNode("This is a paragraph."));
-);
+	 document.getElementById("output").appendChild(newParagraph);
+
 	/*
 	 * Du måste också märka upp paragrafen med klassen even/odd beroende
 	 * på vad den föregående paragrafen märktes upp som
@@ -72,14 +69,13 @@ function addParagraph() {
 	 * vilken av even/odd som väljs)
 	 */
 	 newParagraph.className = ((numParagraphs++ & 1) == 1)?"odd":" even";
-	
-
 }
 
 /*
  * Den här funktionen anropas när man trycker på länken "Räkna bokstäver".
  */
-function countLetters() {
+function countLetters()
+{
 	var count=0;
 
 	/*
@@ -87,7 +83,10 @@ function countLetters() {
 	 * i den nuvarande paragrafen (dvs i din globala textnod).
 	 * Antalet bokstäver sparar du till variabeln 'count'.
 	 */
-
+	 for (var i = currentText.data.length - 1; i >= 0; i--) {
+	 	if(currentText.data[i].toLowerCase() != currentText.data[i].toUpperCase())
+	 		count++;
+	 }
 
 	/*
 	 * Sen avslutar vi med att returnera count variabeln
